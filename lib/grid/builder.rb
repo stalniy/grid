29825b063af7359bbbc8c41f34a658097d9cad34
Sergiy Stotskiy <sergiy.stotskiy@gmail.com>
2013-03-20 20:07:17 +0200

    Update builder.rb

--- lib/grid/builder.rb
+++ lib/grid/builder.rb
@@ -10,42 +10,33 @@ module Grid
       end
 
       %{
-        ::Grid::Builder.assemble(:view_type => ::Grid::Builder::Json, :scope => self){
+        ::Grid::Builder.assemble(:view_type => ::Grid::Builder::Json, :scope => self) {
           #{source}
         }
       }
     end
 
     def self.assemble(options, &block)
-      builder = new(options)
-      block.bind(builder).call if block_given?
-      builder
+      new(options, &block)
     end
 
-    def initialize(options)
+    def initialize(options, &block)
       options.assert_valid_keys(:scope, :view_type)
 
       @_scope = options.delete(:scope)
       @_view_type = options.delete(:view_type)
+      
       copy_instance_variables_from(@_scope) if @_scope
+      self.instance_eval(&block)
     end
 
     def grid_for(relation, options = {}, &block)
-      context = Context.new(options.merge(:scope => @_scope))
-      block.bind(context).call
+      context = Context.new(options.merge(:scope => @_scope), &block)
       @_view_handler = @_view_type.new(relation, context)
     end
-
-    def to_str
-      @_view_handler.assemble_with(@_scope.params)
-    end
     
-    def to_s
-      self.to_str
-    end
-    
-    def handler
-      @_view_handler
+    def assemble(&block)
+      @_view_handler.assemble_with(@_scope.params, &block)
     end
 
     def method_missing(name, *args, &block)
@@ -55,11 +46,14 @@ module Grid
         super
       end
     end
+    
+    def to_s assemble;end
+    def to_str assemble;end
 
   private
 
-    def copy_instance_variables_from(object, exclude = [])
-      vars = object.instance_variables.map(&:to_s) - exclude.map(&:to_s)
+    def copy_instance_variables_from(object)
+      vars = object.instance_variables.map(&:to_s)
       vars.each { |name| instance_variable_set(name.to_sym, object.instance_variable_get(name)) }
     end
 
